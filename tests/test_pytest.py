3af6d7cf5e21a5fa60512f2930b4012df0c367d6
anton-3003 <68605875+anton-3003@users.noreply.github.com>
2021-05-13 09:28:16 +0300

    Update test_pytest.py

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -78,33 +78,33 @@ def test_format_date_invalid(invalid_date):
     assert res is None
 
 
-# def setup():
-#     print("Setup test")
-#
-#
-# def teardown():
-#     print("Teardown test")
-#
-#
-# def setup_module(module):
-#     print("Setup module")
-#
-#
-# def teardown_module(module):
-#     print("Teardown module")
-#
-#
-# def setup_function(function):
-#     print("Setup function")
-#
-#
-# def teardown_function(function):
-#     print("Teardown function")
-#
-#
-# def test_something_one():
-#     print("Test 1")
-#
-#
-# def test_something_two():
-#     print("Test 2")
+def setup():
+    print("Setup test")
+
+
+def teardown():
+    print("Teardown test")
+
+
+def setup_module(module):
+    print("Setup module")
+
+
+def teardown_module(module):
+    print("Teardown module")
+
+
+def setup_function(function):
+    print("Setup function")
+
+
+def teardown_function(function):
+    print("Teardown function")
+
+
+def test_something_one():
+    print("Test 1")
+
+
+def test_something_two():
+    print("Test 2")
